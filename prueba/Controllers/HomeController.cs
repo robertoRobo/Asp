470f35638a5bb2938c4efe829066b02afc8036c0
robertoRobo <robert.tito94@hotmail.com>
2018-01-02 18:20:57 -0600

    Add project files.

---
+++ prueba/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using prueba.Models;
+
+namespace prueba.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // 
+        // GET: /HelloWorld/
+
+        
+
+        // 
+        // GET: /Home/Welcome/ 
+
+        public string Welcome(String name)
+        {
+            //return "This is the Welcome action method... hola: "+name;
+            return System.Text.Encodings.Web.HtmlEncoder.Default.Encode("nane: {name}");
+        }  
+        public IActionResult Index(){
+            return View();
+        }   
+    }
+}
